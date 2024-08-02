4ce8ef979443e66c4fa695449436b7b218e01fda
Andy <andyworm44@gmail.com>
2024-08-03 03:37:11 +0800

    feature:bouns

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, TouchableHighlight, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
+
+  const [bonus, setBouns] = useState(0)
+
+  const get = () => {
+    setBouns(bonus + 100)
+  }
+
+  const reset = () => {
+    setBouns(bonus - bonus)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+      <Text>{bonus}</Text>
+      <TouchableOpacity onPress={() => get()}>
+        <Text>領取100元獎勵！</Text>
+      </TouchableOpacity>
+
+      <TouchableHighlight onPress={() => reset()}>
+        <Text>獎勵歸零！</Text>
+      </TouchableHighlight>
+
+
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mainText: {
+    fontSize: 30,
+    textAlign: 'center',
+    color: 'gray'
+  },
+  secondText: {
+    color: 'green'
+  }
+
 });
